260628d7148a93f44abb8cfb507fb4a6f3f1cd09
bwyfed <36172659@qq.com>
2020-02-06 11:29:02 +0800

    实现爬虫数据的结构设计和存储。

--- src/crowler.ts
+++ src/crowler.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import path from 'path';
 // ts -> .d.ts 翻译文件 -> js
 import superagent from 'superagent';
 import cheerio from 'cheerio';
@@ -5,6 +7,15 @@ import cheerio from 'cheerio';
 interface Course {
   title: string;
   count: number;
+}
+
+interface CourseResult {
+  time: number;
+  data: Course[];
+}
+
+interface Content {
+  [propName: number]: Course[];
 }
 
 class Crowler {
@@ -29,18 +40,35 @@ class Crowler {
         count
       });
     });
-    const result = {
+    return {
       time: new Date().getTime(),
       data: courseInfos
     };
-    console.log(result);
   }
   async getRawHtml() {
     const result = await superagent.get(this.url);
-    this.getCourseInfo(result.text);
+    return result.text;
+  }
+  generateJsonContent(courseInfo: CourseResult) {
+    const filePath = path.resolve(__dirname, '../data/course.json');
+    let fileContent: Content = {};
+    if (fs.existsSync(filePath)) {
+      fileContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+    }
+    fileContent[courseInfo.time] = courseInfo.data;
+    return fileContent;
+    // fileContent是个对象，必须转换为string才能写到文件里。
+    // fs.writeFileSync(filePath, JSON.stringify(fileContent));
+  }
+  async initSpiderProcess() {
+    const filePath = path.resolve(__dirname, '../data/course.json');
+    const html = await this.getRawHtml();
+    const courseInfo = this.getCourseInfo(html);
+    const fileContent = this.generateJsonContent(courseInfo);
+    fs.writeFileSync(filePath, JSON.stringify(fileContent));
   }
   constructor() {
-    this.getRawHtml();
+    this.initSpiderProcess();
   }
 }
 
