086f484029d50db2acf0a26083c810de7a7906ea
bwyfed <36172659@qq.com>
2020-02-09 13:29:13 +0800

    解决使用ts编写常规express代码遇到的问题。

--- src/router.ts
+++ src/router.ts
@@ -5,15 +5,30 @@ import DellAnalyzer from './dellAnalyzer';
 const router = Router();
 
 router.get('/', (req: Request, res: Response) => {
-  res.send('hello world1');
+  res.send(`
+    <html>
+      <body>
+        <form method="post" action="/getData">
+          <input type="password" name="password" />
+          <button type="submit">提交</button>
+        </form>
+      </body>
+    </html>
+  `);
 });
 
-router.get('/getData', (req: Request, res: Response) => {
-  const secret = 'secretKey';
-  const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`;
-  const analyzer = DellAnalyzer.getInstance();
-  new Crowler(url, analyzer);
-  res.send('getData success!');
+router.post('/getData', (req: Request, res: Response) => {
+  console.log(req.body);
+  const { password } = req.body;
+  if (password === '123') {
+    const secret = 'secretKey';
+    const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`;
+    const analyzer = DellAnalyzer.getInstance();
+    new Crowler(url, analyzer);
+    res.send('getData success!');
+  } else {
+    res.send('password error!');
+  }
 });
 
 export default router;
